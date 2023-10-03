465942ac2557aaf9cae61ff5577e3daf5faf716d
Jacob Templeton <jacob.templeton@gmail.com>
2023-10-02 21:41:45 -0700

    Add returned md values from getStaticProps and render them in Home component

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -24,6 +24,20 @@ export default function Home({ allPostsData }) {
         <p>This is my Blog</p>
         <p>You can build a blog like this with Next.js</p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
